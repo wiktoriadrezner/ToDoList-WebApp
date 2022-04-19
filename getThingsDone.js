7da363e1128e3c265371eb0c046447caa17ec535
Wiktoria Drezner <93138210+wiktoriadrezner@users.noreply.github.com>
2022-04-19 22:42:49 +0200

    Aesthetics

--- getThingsDone.js
+++ getThingsDone.js
@@ -1,17 +1,40 @@
 document.querySelector("#idAddButton").onclick = function () {
+  // Add New Task to the List
   document.querySelector("#idListTasksToDo").innerHTML += `
-            <div class="taskToDo">
-                    ${document.querySelector("#idAddTask input").value}
-                <button class="delete">
-                    <i class="far fa-trash-alt"></i>
-                </button>
-            </div>
-        `;
+    <div class="taskToDo">
+        ${document.querySelector("#idAddTask input").value}
+        <button class="buttonCompleteTask">✔️</button>
+        <button class="buttonDeleteTask">❌</button>
+    </div>
+    `;
 
-  var current_tasks = document.querySelectorAll(".delete");
-  for (var i = 0; i < current_tasks.length; i++) {
-    current_tasks[i].onclick = function () {
+  // Delete Tasks
+  var taskToDelete = document.querySelectorAll(".buttonDeleteTask");
+  for (var i = 0; i < taskToDelete.length; i++) {
+    taskToDelete[i].onclick = function () {
       this.parentNode.remove();
+    };
+  }
+
+  // Move Tasks to Completed
+  var taskCompleted = document.querySelectorAll(".buttonCompleteTask");
+  for (var i = 0; i < taskCompleted.length; i++) {
+    taskCompleted[i].onclick = function () {
+      this.parentNode.remove();
+      // Add Completed Tasks to the List
+      document.querySelector("#idListTasksDone").innerHTML += `
+        <div class="taskDone">
+            ${document.querySelector("#idAddTask input").value}
+            <button class="buttonDeleteDoneTask">❌</button>
+        </div>
+        `;
+      // Delete Completed Task
+      var taskToDelete = document.querySelectorAll(".buttonDeleteDoneTask");
+      for (var i = 0; i < taskToDelete.length; i++) {
+        taskToDelete[i].onclick = function () {
+          this.parentNode.remove();
+        };
+      }
     };
   }
 };
